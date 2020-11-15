af2ac6c1dd849a69caa5b6b6f2271650c868ef28
holasebas <danielpalma2003@hotmail.com>
2020-11-15 07:49:30 -0500

    new interface roles for proyects model

---
+++ src/interfaces/roles.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Danny Palma
+ * @abstract Exports the interface roles for the proyects
+ */
+
+export default interface Roles {
+    permissions: 1 | 2 | 3 | 4;
+    user_id: string;
+};
+/**
+ * Permissions:
+ * 1 = quiere entrar al proyecto
+ * 2 = esta adentro del proyecto
+ * 3 = es moderador del proyecto
+ * 4 = es el owner del proyecto
+ */
