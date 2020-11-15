17cb8902ccb5b7c42368ad249276a9d34c121640
holasebas <danielpalma2003@hotmail.com>
2020-11-14 23:09:17 -0500

    added errors handlers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,22 @@
  * 
  */
 
-import { Application } from "express";
+import { Application, Request, Response, ErrorRequestHandler, NextFunction } from "express";
 import routerMain from "./main/main.router";
 
 export default class routes {
     constructor(private app: Application) {
         this.app.use(routerMain);
+        this.errors();
+    };
+    private errors() {
+        // Error 404
+        this.app.use((req: Request, res: Response) => {
+            return res.status(404).json({ error: 'the page noesn\'t exist' });
+        });
+        // Error 500
+        this.app.use((error: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+            return res.status(500).json({ error: 'something wrong :(' });
+        });
     };
 };
