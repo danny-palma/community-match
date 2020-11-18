4d24ba36d3525bd7324848879d2f202ecbc4c9e5
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-18 15:50:26 -0500

    added dev routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,22 @@
 /**
  * @author Danny Palma
- * @abstract Creates the routes
- * 
+ * @fileoverview Creates the routes
  */
 
 import { Application, Request, Response, ErrorRequestHandler, NextFunction } from "express";
 import routerMain from "./main/main.router";
+import routerUsers from "./users/loggin.router";
+import routerGui from "./gui/gui.router";
 
 export default class routes {
     constructor(private app: Application) {
         this.app.use(routerMain);
+        this.app.use('/users', routerUsers);
+        if(process.env.NODE_ENV != 'production') this.developmentRoutes();
         this.errors();
+    };
+    private developmentRoutes(){
+        this.app.use('/gui', routerGui);
     };
     private errors() {
         // Error 404
@@ -19,7 +25,9 @@ export default class routes {
         });
         // Error 500
         this.app.use((error: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
-            return res.status(500).json({ error: 'something wrong :(' });
+            console.log(error)
+            res.status(500).json({ error: 'something wrong :(', details: error.toString() });
+            return next();
         });
     };
 };
