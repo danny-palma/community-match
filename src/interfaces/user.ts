f94c790af3132047b70964a6d1e4a31a20cbec2c
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-18 15:48:47 -0500

    now notifications have type

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,21 +1,22 @@
 /** 
  * @author Danny Palma
- * @abstract Exports the interface user for the database
+ * @fileoverview Exports the interface user for the database
  */
 
 import { Document } from "mongoose";
 import skills from "./skills";
 
 interface notifications {
-    title: string,
-    timeStamp: Date,
-    body: string,
+    title: string;
+    timeStamp: Date;
+    body: string;
+    type: 'danger' | 'warning' | 'alert' | 'social' | 'projects' | 'news';
 };
 
 interface previewProject {
-    name: string,
-    description: string,
-    projectID: string,
+    name: string;
+    description: string;
+    projectID: string;
 };
 
 export default interface User extends Document {
