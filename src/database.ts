40c13f315fcb56b836c50fde3e2fa63bb3bbe24c
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-18 15:45:40 -0500

    added db connection

--- src/database.ts
+++ src/database.ts
@@ -1,1 +1,20 @@
-// todo: create a database
+/**
+ * @author Danny Palma
+ * @fileoverview Create a database connection
+ */
+
+import { connect } from "mongoose";
+import config from "./config";
+
+let uri = process.env.NODE_ENV == 'production' ? config.DBURI : config.devDBURI;
+
+export default async () => {
+    return await connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }, (err) => {
+        if (err) throw err;
+        console.log(`database is connected to ${uri}`);
+    });
+};
