115d2834e7f4f037015bb360a4fef5e0b183515b
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-19 07:57:57 -0500

    added exeption handlers

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+process.on('unhandledRejection', (_reason, promise) => {
+    console.log('ocurrio una exepcion inesperada en: ', promise);
+});
+process.on('uncaughtException', (error) => {
+    console.log('ocurrio una exepcion inesperada: ', error);
+    process.exit();
+});
+
 import App from './app';
 
 let app = new App();
