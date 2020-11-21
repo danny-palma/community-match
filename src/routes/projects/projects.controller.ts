c42c8c28f3f29466ffaa0a515c0ded9ff08c0cb9
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-21 08:06:48 -0500

    new routes

--- src/routes/projects/projects.controller.ts
+++ src/routes/projects/projects.controller.ts
@@ -1,9 +1,33 @@
 import { Request, Response } from "express";
 import adminUsers from "../../administators/admin-users";
+import adminProject from "../../administators/admin-projects";
 
 export async function newProject(req: Request, res: Response) {
     if (!req.headers.authorization) return res.status(403).json({ error: 'you don\'t provide authorization code' });
     if (!adminUsers.existKey(req.headers.authorization)) return res.status(403).json({ error: 'the authorization code is invalid' });
-    
-    res.send({ status: 'ok' });
+    console.log(req.body);
+    try {
+        res.send(await adminProject.newProject(req.headers.authorization, {
+            description: req.body.description,
+            links: req.body.links,
+            name: req.body.name,
+            reqiredSkills: req.body.required_skills,
+            roles: req.body.roles
+        }));
+    } catch (err) {
+        res.status(403).json({ error: err })
+    }
+
 };
+
+export async function deleteProject(req: Request, res: Response) {
+    if (!req.headers.authorization) return res.status(403).json({ error: 'you don\'t provide authorization code' });
+    if (!adminUsers.existKey(req.headers.authorization)) return res.status(403).json({ error: 'the authorization code is invalid' });
+    if (!req.body.project_ID) return res.status(403).json({ error: 'you don\'t provide project id' });
+    try {
+        res.send(await adminProject.deleteProject(req.headers.authorization, req.body.project_ID))
+    } catch (err) {
+        console.log('error', err)
+        res.status(304).json({ error: err });
+    }
+}
