10cd6cf388012e7a884ea174b9eee3848da811f4
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-11-24 21:35:12 -0500

    added express server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-process.on('unhandledRejection', (_reason, promise) => {
+/* process.on('unhandledRejection', (_reason, promise) => {
     console.log('ocurrio una exepcion inesperada en: ', promise);
 });
 process.on('uncaughtException', (error) => {
@@ -10,4 +10,22 @@ import App from './app';
 
 let app = new App();
 
-app.listen();
+app.listen(); */
+
+const express = require('express');
+const expbhs = require('express-handlebars')
+const path = require('path')
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('views')
+app.engine('hsb', expbhs({
+
+}))
+
+// Start Server
+app.listen(3000, () => {
+    console.log('Server on port', 3000)
+})
